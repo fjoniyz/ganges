14f7b3f21e3d73a9765d806d98cd3b71a6a05370
Maxim Popov <itsmail4max@gmail.com>
2023-05-12 01:09:00 +0200

    Simplified pom.xml

---
+++ src/main/java/interceptorExample/DemoProducerInterceptor.java
@@ -0,0 +1,30 @@
+package interceptorExample;
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Map;
+
+public class DemoProducerInterceptor implements ProducerInterceptor<String,String>{
+    @Override
+    public ProducerRecord<String, String> onSend(ProducerRecord<String, String> producerRecord) {
+        ProducerRecord<String, String> mutatedRecord =
+                new ProducerRecord<>(producerRecord.topic(), producerRecord.key(), producerRecord.value().replaceAll("e", "3").replaceAll("o", "0"));
+        return mutatedRecord;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata recordMetadata, Exception e) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
