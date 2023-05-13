e662cd2fdc16aa3c3f2a2e072bbadc3b3a35bf6d
fjoni <fjoniyzeiri@gmail.com>
2023-05-13 17:12:43 +0200

    linting

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -10,19 +10,16 @@ import org.apache.kafka.streams.kstream.KStream;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-/**
- * In this example, we implement a simple Pipe program using the high-level Streams DSL
- * that reads from a source topic "streams-plaintext-input", where the values of messages represent lines of text,
- * and writes the messages as-is into a sink topic "streams-pipe-output".
- */
-public class Pipe {
+class Pipe {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+                Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+                Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
         KStream<String, String> src = builder.stream("streams-plaintext-input");
