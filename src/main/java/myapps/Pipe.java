d6b8cf15e56025276c9b67e5fa88e6ab52af2373
fjoni <fjoniyzeiri@gmail.com>
2023-05-14 21:46:25 +0200

    formatting

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -2,7 +2,6 @@ package myapps;
 
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
-
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -12,27 +11,27 @@ import org.apache.kafka.streams.kstream.KStream;
 
 class Pipe {
 
-    public static void main(final String[] args) {
-	Properties props = new Properties();
-	props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-	props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-	props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-	props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+  public static void main(final String[] args) {
+    Properties props = new Properties();
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-	final StreamsBuilder builder = new StreamsBuilder();
-	KStream<String, String> src = builder.stream("streams-plaintext-input");
-	src.mapValues(value -> value + "X").to("streams-pipe-output");
+    final StreamsBuilder builder = new StreamsBuilder();
+    KStream<String, String> src = builder.stream("streams-plaintext-input");
+    src.mapValues(value -> value + "X").to("streams-pipe-output");
 
-	final Topology topology = builder.build();
-	final KafkaStreams streams = new KafkaStreams(topology, props);
-	final CountDownLatch latch = new CountDownLatch(1);
+    final Topology topology = builder.build();
+    final KafkaStreams streams = new KafkaStreams(topology, props);
+    final CountDownLatch latch = new CountDownLatch(1);
 
-	try {
-	    streams.start();
-	    latch.await();
-	} catch (Throwable e) {
-	    System.exit(1);
-	}
-	System.exit(0);
+    try {
+      streams.start();
+      latch.await();
+    } catch (Throwable e) {
+      System.exit(1);
     }
+    System.exit(0);
+  }
 }
