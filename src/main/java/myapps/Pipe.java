8b395efaedf04da680e33debdfea39692fe36ebf
Fjoni Yzeiri <fjoniyzeiri@gmail.com>
2023-05-21 19:11:17 +0200

    Initial Dockerfile for kafka streams file (#25)
    * Initial Dockerfile for kafka streams file
    
    * Added docker-compose and env-interface
    
    * add simple data gen
    
    * Fix: output topic not initially writeable
    
    ---------
    
    Co-authored-by: flow <git@lowspace.de>
    Co-authored-by: Maxim Popov <itsmail4max@gmail.com>
    Co-authored-by: flow <19375496+overflw@users.noreply.github.com>

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -16,26 +16,35 @@ import org.apache.kafka.streams.kstream.KStream;
  */
 public class Pipe {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     Properties props = new Properties();
+    String inputTopic = EnvTools.getEnvValue(EnvTools.INPUT_TOPIC, "streams-input");
+    String outputTopic = EnvTools.getEnvValue(EnvTools.OUTPUT_TOPIC, "streams-output");
+    String bootstrapServerConfig =
+        EnvTools.getEnvValue(EnvTools.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
     props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    props.put(
+        StreamsConfig.METADATA_MAX_AGE_CONFIG,
+        "1000"); // Needed to prevent timeouts during broker startup.
 
     final StreamsBuilder builder = new StreamsBuilder();
-    KStream<String, String> src = builder.stream("streams-plaintext-input");
-    src.mapValues(value -> value + "X").to("streams-pipe-output");
+    KStream<String, String> src = builder.stream(inputTopic);
+    src.mapValues(value -> value + "X").to(outputTopic);
 
     final Topology topology = builder.build();
-    final KafkaStreams streams = new KafkaStreams(topology, props);
-    final CountDownLatch latch = new CountDownLatch(1);
+    try (KafkaStreams streams = new KafkaStreams(topology, props)) {
+      final CountDownLatch latch = new CountDownLatch(1);
 
-    try {
-      streams.start();
-      latch.await();
-    } catch (Throwable e) {
-      System.exit(1);
+      try {
+        streams.start();
+        latch.await();
+      } catch (Throwable e) {
+        System.exit(1);
+      }
     }
     System.exit(0);
   }
