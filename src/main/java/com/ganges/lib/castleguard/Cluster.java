4ab8eb725d05998dd63ea3861eb7e7e8d652dea5
sofiagreten123 <s.greten@campus.tu-berlin.de>
2023-05-21 19:00:38 +0200

    implemented first few methods in Cluster.java and Item.java

---
+++ src/main/java/com/ganges/lib/castleguard/Cluster.java
@@ -0,0 +1,147 @@
+package com.ganges.lib.castleguard;
+
+import org.apache.commons.lang3.Range;
+
+import java.util.*;
+import java.lang.String;
+import java.lang.Float;
+
+public class Cluster {
+    List<Item> contents;
+    Map<String, Range<Integer>> ranges;
+    // TODO: change type Object
+    Set<Object> diversity;
+    Map<String, Float> sample_values;
+
+    public Cluster(List<String> headers){
+        // Initialises the cluster
+        this.contents= new ArrayList<>();
+        this.ranges = new HashMap<>();
+        // TODO: research about the Ranges method
+        headers.forEach(header -> {
+            this.ranges.put(header, new Range<Integer>());
+        });
+        this.diversity = new HashSet();
+        this.sample_values = new HashMap<>();
+    }
+    void insert(Item element){
+        // Inserts a tuple into the cluster
+        // Args:
+        //            element (Item): The element to insert into the cluster
+        this.contents.add(element);
+
+        // Check whether the item is already in a cluster
+        if (element.parent != null){
+            // If it is, remove it so that we do not reach an invalid state
+            element.parent.remove(element);
+        }
+        // Add sensitive attribute value to the diversity of cluster
+        this.diversity.add(element.sensitive_attr);
+        element.parent = this;
+    }
+
+    void remove(Item element){
+       //  Removes a tuple from the cluster
+       //  Args:
+        //            element: The element to remove from the cluster
+        this.contents.remove(element);
+        for(Item e : this.contents){
+            if (! (Objects.equals(e.sensitive_attr, element.sensitive_attr))) {
+                this.diversity.remove(element.sensitive_attr);
+            }
+
+        }
+    }
+    // TODO no void functions -> returns pd.series in python
+     void generalise(){
+
+        }
+
+     float tuple_enlargement(Item item, HashMap<String, Range> global_ranges){
+            /*Calculates the enlargement value for adding <item> into this cluster
+
+        Args:
+            item: The tuple to calculate enlargement based on
+            global_ranges: The globally known ranges for each attribute
+
+    Returns: The information loss if we added item into this cluster
+           */
+        Float given = this.information_loss_given_t(item, global_ranges);
+        Float current = this.information_loss(global_ranges);
+        return (given - current) /this.ranges.size();
+    }
+
+    float cluster_enlargement(Cluster cluster, HashMap<String, Range> global_ranges){
+        /*Calculates the enlargement value for merging <cluster> into this cluster
+
+        Args:
+        cluster: The cluster to calculate information loss for
+        global_ranges: The globally known ranges for each attribute
+
+        Returns: The information loss upon merging cluster with this cluster*/
+        return 1.0F;
+    }
+
+    float information_loss_given_t(Item item, HashMap<String, Range> global_ranges){
+        /*Calculates the information loss upon adding <item> into this cluster
+
+        Args:
+        item: The tuple to calculate information loss based on
+        global_ranges: The globally known ranges for each attribute
+
+        Returns: The information loss given that we insert item into this cluster*/
+
+        return 1.0F;
+    }
+
+    float information_loss_given_c(Cluster cluster, HashMap<String, Range> global_ranges) {
+       /* Calculates the information loss upon merging <cluster> into this cluster
+
+        Args:
+            cluster: The cluster to calculate information loss based on
+            global_ranges: The globally known ranges for each attribute
+
+        Returns: The information loss given that we merge cluster with this cluster*/
+        return 1.0F;
+    }
+
+    float information_loss(HashMap<String, Range> global_ranges){
+        /*Calculates the information loss of this cluster
+
+        Args:
+        global_ranges: The globally known ranges for each attribute
+
+        Returns: The current information loss of the cluster*/
+        return 1.0F;
+    }
+
+    float distance(Item other){
+        /*Calculates the distance from this tuple to another
+
+        Args:
+        other: The tuple to calculate the distance to
+
+        Returns: The distance to the other tuple*/
+        return 1.0F;
+    }
+
+    boolean within_bounds(Item item){
+       /* Checks whether a tuple is within all the ranges of the this
+        cluster, eg. would cause no information loss on being entered.
+
+                Args:
+        item: The tuple to perform bounds checking on
+
+        Returns: Whether or not the tuple is within the bounds of the cluster*/
+
+        for (header, header_range in this.ranges.items()){
+            if (!header_range.within_bounds(item[header])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
--- src/main/java/com/ganges/lib/castleguard/Item.java
+++ src/main/java/com/ganges/lib/castleguard/Item.java
@@ -1,5 +1,36 @@
 package com.ganges.lib.castleguard;
+import java.util.*;
 
 public class Item {
+    Map<String, Float> data;
+    List<String> headers;
+    Float sensitive_attr;
+    // TODO: is this Cluster or Item Type?
+    Cluster parent = null;
 
+    public Item(Map <String, Float> data, List <String>  headers, String sensitive_attr){
+        this.data  = data;
+        this.headers = headers;
+        this.sensitive_attr = data.get(sensitive_attr);
+        this.parent = null;
+    }
+    float tuple_distance(Item other){
+        /*Calculates the distance between the two tuples
+        Args:
+        other: The tuple to calculate the distance to
+        Returns: The distance to the tuple*/
+
+        Float error = Math.abs(this.data.get(this.headers) - other.data.get(this.headers));
+        // TODO: noch Mean Value darauf anwenden
+        Float mean_squared_error = (float) Math.pow(error, 2);
+        return (float) Math.sqrt(mean_squared_error);
+    }
+
+    void update_attribute(String header, Float value) {
+        /*Updates a value in the tuple's data
+        Args:
+            header: The header to change
+            value: The value to change to*/
+        this.data.replace(header, value);
+    }
 }
