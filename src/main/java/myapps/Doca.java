9926ec2e527b9a989b3151ea26d498125224b323
fjoni <fjoniyzeiri@gmail.com>
2023-05-24 19:15:35 +0200

    Doca now gets executed in streams~

--- src/main/java/myapps/Doca.java
+++ src/main/java/myapps/Doca.java
@@ -1,15 +1,13 @@
 package myapps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import java.io.*;
-import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Predicate;
-import java.util.stream.IntStream;
-
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 
 public class Doca {
 
@@ -33,8 +31,16 @@ public class Doca {
     return min;
   }
 
-  public static Double[] div0(double[] a, double[] b) {
-    Double[] result = new Double[a.length];
+  public static double getMean(List<Double> values) {
+    double sum = 0;
+    for (double value : values) {
+      sum += value;
+    }
+    return sum / values.size();
+  }
+
+  public static double[] div0(double[] a, double[] b) {
+    double[] result = new double[a.length];
     for (int i = 0; i < a.length; i++) {
       result[i] = (b[i] != 0) ? a[i] / b[i] : 0;
     }
@@ -49,12 +55,13 @@ public class Doca {
     return sum;
   }
 
-  public double[][] doca(
-      double[][] X, double eps, int delay_constraint, int beta, int mi, boolean inplace) {
+  public static double[][] doca(
+          double[][] X, double eps, int delay_constraint, int beta, int mi, boolean inplace) {
     int num_instances = X.length;
     int num_attributes = X[0].length;
 
-    double sensitivity = 1.5 * (getMax(X) - getMin(X));
+    double sensitivity =
+            1.5 * (getMax(X) - getMin(X));
 
     List<List<Integer>> clusters = new ArrayList<>();
     List<List<Integer>> clusters_final = new ArrayList<>();
@@ -113,8 +120,8 @@ public class Doca {
           double sum = 0;
           for (int i = 0; i < num_attributes; i++) {
             sum +=
-                Math.max(0, data_point[i] - mx_c.get(c)[i])
-                    - Math.min(0, data_point[i] - mn_c.get(c)[i]);
+                    Math.max(0, data_point[i] - mx_c.get(c)[i])
+                            - Math.min(0, data_point[i] - mn_c.get(c)[i]);
           }
           enlargement[c] = sum;
         }
@@ -138,16 +145,16 @@ public class Doca {
         if (!ok_clusters.isEmpty()) {
           TODOREMOVE_Perfect += 1;
           best_cluster =
-              ok_clusters.stream()
-                  .min(
-                      (c1, c2) -> Integer.compare(clusters.get(c1).size(), clusters.get(c2).size()))
-                  .orElse(null);
+                  ok_clusters.stream()
+                          .min(
+                                  (c1, c2) -> Integer.compare(clusters.get(c1).size(), clusters.get(c2).size()))
+                          .orElse(null);
         } else if (clusters.size() >= beta) {
           best_cluster =
-              min_clusters.stream()
-                  .min(
-                      (c1, c2) -> Integer.compare(clusters.get(c1).size(), clusters.get(c2).size()))
-                  .orElse(null);
+                  min_clusters.stream()
+                          .min(
+                                  (c1, c2) -> Integer.compare(clusters.get(c1).size(), clusters.get(c2).size()))
+                          .orElse(null);
         }
       }
 
@@ -165,7 +172,7 @@ public class Doca {
         // Update min/max
         double[] mn_cluster = mn_c.get(best_cluster);
         double[] mx_cluster = mx_c.get(best_cluster);
-        //        for (int i = 0; i < num_attributes; i++) {
+//        for (int i = 0; i < num_attributes; i++) {
         mn_cluster[best_cluster] = Math.min(mn_cluster[best_cluster], data_point[best_cluster]);
         mx_cluster[best_cluster] = Math.max(mx_cluster[best_cluster], data_point[best_cluster]);
       }
@@ -177,7 +184,7 @@ public class Doca {
         }
       }
       assert overripe_clusters.size() <= 1
-          : "Every datapoint should only be able to be in one cluster!?";
+              : "Every datapoint should only be able to be in one cluster!?";
       if (!overripe_clusters.isEmpty()) {
         int c = overripe_clusters.get(0);
         double[] dif_cluster = new double[num_attributes];
@@ -222,21 +229,21 @@ public class Doca {
     List<Double[]> dataList = readCSVFile("./adult_train.csv");
 
     List<String> columns =
-        Arrays.asList(
-            "age",
-            "education-num",
-            "marital-status",
-            "gender",
-            "capital-gain",
-            "hours-per-week",
-            "income");
+            Arrays.asList(
+                    "age",
+                    "education-num",
+                    "marital-status",
+                    "gender",
+                    "capital-gain",
+                    "hours-per-week",
+                    "income");
     Double[][] data = extractColumns(dataList, columns);
 
     mapIncomeColumn(data);
 
     double[][] normalizedData = normalizeDataFrame(data);
 
-    // double[][] res = doca(normalizedData, 100, 1000, 50, 100, false);
+    double[][] res = doca(normalizedData, 100, 1000, 50, 100, false);
 
     printResult(res);
   }
@@ -284,7 +291,7 @@ public class Doca {
   private static double[] extractColumns(Double[][] data, int index) {
     double[] res = new double[data.length];
     int k = 0;
-    for (Double[] rows : data) {
+    for(Double[] rows: data){
       res[k] = rows[index];
       k++;
     }
@@ -320,260 +327,5 @@ public class Doca {
       k++;
       System.out.println();
     }
-  }
-  public static double sum(double[] input){
-    double sum = 0;
-    for(double d: input){
-      sum += d;
-    }
-    return sum;
-  }
-  public static ArrayList<double[]> zip(double[] a, double[] b) {
-    ArrayList<double[]> res = new ArrayList<>();
-    int maxLength = Math.min(a.length, b.length);
-    for(int i = 0; i < maxLength; i++){
-      res.add(new double[] {a[i], b[i]});
-    }
-    return res;
-  }
-
-  public static double[] vectorize(
-          BiFunction<Double, Double, Double> function, double[] a, double[] b) {
-    Predicate<Integer> nonZero = value -> value != 0;
-    return IntStream.range(0, Math.min(a.length, b.length))
-            .mapToDouble(i -> function.apply(a[i], b[i]))
-            .toArray();
-  }
-
-  public static double[][] doca(
-      double[][] X, double eps, int delay_constraint, int beta, int mi, boolean inplace) {
-    int num_instances = X.length;
-    int num_attributes = X[0].length;
-    BiFunction<Double, Double, Double> divisionFunction = (a, b) -> b != 0 ? (double) a / b : 0;
-
-    double sensitivity = 1.5 * (getMax(X) - getMin(X));
-
-    List<List<Integer>> clusters = new ArrayList<>();
-    List<List<Integer>> clusters_final = new ArrayList<>();
-
-    // Cluster attribute minimum/maximum
-    List<double[]> mn_c = new ArrayList<>();
-    List<double[]> mx_c = new ArrayList<>();
-
-    // Global Attribute Minimum/Maximum
-    double[] mn = new double[num_attributes];
-    double[] mx = new double[num_attributes];
-    for (int i = 0; i < num_attributes; i++) {
-      mn[i] = Double.POSITIVE_INFINITY;
-      mx[i] = Double.NEGATIVE_INFINITY;
-    }
-
-    // Losses saved for tau
-    List<Double> losses = new ArrayList<>();
-
-    double tau = 0;
-
-    // Create Output structure
-    double[][] output;
-    if (inplace) {
-      output = X;
-    } else {
-      output = new double[num_instances][num_attributes];
-    }
-
-    int TODOREMOVE_Perfect = 0;
-
-    for (int clock = 0; clock < num_instances; clock++) {
-      if (clock % 1000 == 0) {
-        System.out.println("Clock " + clock + " " + TODOREMOVE_Perfect);
-      }
-
-      double[] data_point = X[clock];
-
-      // Update min/max
-      for (int i = 0; i < num_attributes; i++) {
-        mn[i] = Math.min(mn[i], data_point[i]);
-        mx[i] = Math.max(mx[i], data_point[i]);
-      }
-
-      double[] dif = new double[num_attributes];
-      for (int i = 0; i < num_attributes; i++) {
-        dif[i] = mx[i] - mn[i];
-      }
-
-      // Find best Cluster
-      Integer best_cluster = null;
-      if (!clusters.isEmpty()) {
-        ArrayList<ArrayList<double[]>> zippedEntries = new ArrayList<>();
-        for(int i = 0; i < mn_c.size(); i++){
-          zippedEntries.set(i, zip(mn_c.get(i), mx_c.get(i)));
-        }
-        // Calculate enlargement (the value is not yet divided by the number of attributes!)
-        ArrayList<Double[]> enlargement = new ArrayList<>();
-        double[] zeros = new double[zippedEntries.size()];
-        for (int c = 0; c < clusters.size(); c++) {
-          double sum = 0;
-          for (int i = 0; i < num_attributes; i++) {
-            enlargement.set(i,
-                div0(maximumArray(zeros, subtractArrays(data_point, zippedEntries.get(c).get(i))), dif));
-          }
-        }
-
-        double min_enlarge = 30;
-
-        List<Integer> ok_clusters = new ArrayList<>();
-        List<Integer> min_clusters = new ArrayList<>();
-
-        for (int c = 0; c < clusters.size(); c++) {
-          double enl = enlargement.get(c);
-          if (enl == min_enlarge) {
-            min_clusters.add(c);
-            double overall_loss = (enl + getSum(div0(mx_c.get(c), dif))) / num_attributes;
-            if (overall_loss <= tau) {
-              ok_clusters.add(c);
-            }
-          }
-        }
-
-        if (!ok_clusters.isEmpty()) {
-          TODOREMOVE_Perfect += 1;
-          //Finds the smallest item in ok_clusters
-          best_cluster =
-              ok_clusters.stream()
-                  .min(
-                          Comparator.comparingInt(c -> clusters.get(c).size()))
-                  .orElse(null);
-        } else if (clusters.size() >= beta) {
-          //Find the smallest item in min_clusters
-          best_cluster =
-              min_clusters.stream()
-                  .min(
-                          Comparator.comparingInt(c -> clusters.get(c).size()))
-                  .orElse(null);
-        }
-      }
-
-      if (best_cluster == null) {
-        // Add new Cluster
-        List<Integer> new_cluster = new ArrayList<>();
-        new_cluster.add(clock);
-        clusters.add(new_cluster);
-        // Set Min/Max of new Cluster
-        mn_c.add(data_point.clone());
-        mx_c.add(data_point.clone());
-      } else {
-        clusters.get(best_cluster).add(clock);
-        // Update min/max
-        double[] mn_cluster = mn_c.get(best_cluster);
-        double[] mx_cluster = mx_c.get(best_cluster);
-        for (int i = 0; i < num_attributes; i++) {
-          mn_cluster[best_cluster] = Math.min(mn_cluster[best_cluster], data_point[best_cluster]);
-          mx_cluster[best_cluster] = Math.max(mx_cluster[best_cluster], data_point[best_cluster]);
-        }
-      }
-
-      List<Integer> overripe_clusters = new ArrayList<>();
-      for (int c = 0; c < clusters.size(); c++) {
-        if (clusters.get(c).contains(clock - delay_constraint)) {
-          overripe_clusters.add(c);
-        }
-      }
-      assert overripe_clusters.size() <= 1
-          : "Every datapoint should only be able to be in one cluster!?";
-      if (!overripe_clusters.isEmpty()) {
-        int c = overripe_clusters.get(0);
-        double[] dif_cluster = new double[num_attributes];
-        for (int i = 0; i < num_attributes; i++) {
-          dif_cluster[i] = mx_c.get(c)[i] - mn_c.get(c)[i];
-        }
-        double loss = getSum(div0(dif_cluster, dif)) / num_attributes;
-        losses.add(loss);
-        clusters_final.add(clusters.get(c));
-        clusters.remove(c);
-        mn_c.remove(c);
-        mx_c.remove(c);
-      }
-    }
-
-    clusters_final.addAll(clusters);
-
-    for (List<Integer> cs : clusters_final) {
-      double[] mean = new double[num_attributes];
-      for (int i : cs) {
-        for (int j = 0; j < num_attributes; j++) {
-          mean[j] += X[i][j];
-        }
-      }
-      for (int j = 0; j < num_attributes; j++) {
-        mean[j] /= cs.size();
-      }
-      double scale = (sensitivity / (cs.size() * eps));
-      double[] laplace = new double[num_attributes];
-      for (int j = 0; j < num_attributes; j++) {
-        laplace[j] = Math.random() - 0.5;
-      }
-      for (int j = 0; j < num_attributes; j++) {
-        output[cs.get(0)][j] = mean[j] + scale * laplace[j];
-      }
-    }
-
-    return output;
-  }
-
-  public static double[] subtractArrays(double[] first, double[] second) {
-    int min_length = Math.min(first.length, second.length);
-    double[] result = new double[min_length];
-    for(int i = 0; i < min_length; i++){
-      result[i] = first[i] - second[i];
-    }
-    return result;
-  }
-
-  public static double[] maximumArray(double[]a, double[]b) {
-    int min_length = Math.min(a.length, b.length);
-    int max_length = Math.max(a.length, b.length);
-    int difference = max_length - min_length;
-    double[] results_arr = new double[max_length];
-    for(int i = 0; i < min_length; i++){
-      results_arr[i] = Math.max(a[i], b[i]);
-    }
-    if(a.length > b.length){
-      if (max_length - difference >= 0)
-        System.arraycopy(a, difference, results_arr, difference, max_length - difference);
-    }else{
-      if (max_length - difference >= 0)
-        System.arraycopy(b, difference, results_arr, difference, max_length - difference);
-    }
-    return results_arr;
-  }
-
-  public static double[] fill(double[] arrayToFill, int startIndex, int endIndex, double value){
-    for(int i = startIndex; i < endIndex; i++){
-      arrayToFill[i] = value;
-    }
-    return arrayToFill;
-  }
-
-  public static void main(String[] args) {
-    List<Double[]> dataList = readCSVFile("./adult_train.csv");
-
-    List<String> columns =
-        Arrays.asList(
-            "age",
-            "education-num",
-            "marital-status",
-            "gender",
-            "capital-gain",
-            "hours-per-week",
-            "income");
-    Double[][] data = extractColumns(dataList, columns);
-
-    mapIncomeColumn(data);
-
-    double[][] normalizedData = normalizeDataFrame(data);
-
-    double[][] res = doca(normalizedData, 100, 1000, 50, 100, false);
-
-    printResult(res);
   }
 }
--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -1,5 +1,10 @@
 package myapps;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import org.apache.kafka.common.serialization.Serdes;
@@ -8,6 +13,8 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * In this example, we implement a simple Pipe program using the high-level Streams DSL that reads
@@ -17,56 +24,63 @@ import org.apache.kafka.streams.kstream.KStream;
 public class Pipe {
 
   public static void main(final String[] args) {
-    Properties props = new Properties();
-    String inputTopic = EnvTools.getEnvValue(EnvTools.INPUT_TOPIC, "streams-input");
-    String outputTopic = EnvTools.getEnvValue(EnvTools.OUTPUT_TOPIC, "streams-output");
-    String bootstrapServerConfig =
-        EnvTools.getEnvValue(EnvTools.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    String userDirectory = System.getProperty("user.dir");
+    try (InputStream inputStream =
+        Files.newInputStream(Paths.get(userDirectory + "/src/main/resources/config.properties"))) {
+      Properties props = new Properties();
 
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
-    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    props.put(
-        StreamsConfig.METADATA_MAX_AGE_CONFIG,
-        "1000"); // Needed to prevent timeouts during broker startup.
+      String inputTopic = "input";
+      String outputTopic = "output";
 
-    final StreamsBuilder builder = new StreamsBuilder();
-    KStream<String, String> src = builder.stream(inputTopic);
-    src.mapValues(value -> {
-      double[] values = 
-      Doca d = new Doca();
-      d.doca(value, );
-    }).to(outputTopic);
+      props.load(inputStream);
+      props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+      props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+      props.put(
+          StreamsConfig.METADATA_MAX_AGE_CONFIG,
+          "1000"); // Needed to prevent timeouts during broker startup.
 
-    final Topology topology = builder.build();
-    try (KafkaStreams streams = new KafkaStreams(topology, props)) {
-      final CountDownLatch latch = new CountDownLatch(1);
+      final StreamsBuilder builder = new StreamsBuilder();
+      KStream<String, String> src = builder.stream(inputTopic);
+      src.mapValues(
+              value -> {
+                String[] values = value.split(",");
+                System.out.println("Values: " + Arrays.toString(values));
+                List<String> strings = Arrays.asList(values);
+                System.out.println("We get here too");
+                List<Double> listOfDoubles =
+                    strings.stream().map(Double::valueOf).collect(Collectors.toList());
+                System.out.println("What about here");
+                double[] arr = listOfDoubles.stream().mapToDouble(Double::doubleValue).toArray();
+                double[][] input = new double[][] {arr, arr};
+                System.out.println("Input: " + input);
+                double[][] output = Doca.doca(input, 100, 1000, 60, 100, false);
+                System.out.println("Output: " + output);
+                String result = Arrays.deepToString(output);
+                System.out.println("Result: " + result);
+                return result;
+              })
+          .to(outputTopic);
 
-      try {
-        streams.start();
-        latch.await();
-      } catch (Throwable e) {
-        System.exit(1);
+      final Topology topology = builder.build();
+      try (KafkaStreams streams = new KafkaStreams(topology, props)) {
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        try {
+          streams.start();
+          latch.await();
+        } catch (Throwable e) {
+          System.exit(1);
+        }
       }
+      System.exit(0);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-    System.exit(0);
   }
-  private static double[] createValues (String valuesString) {
-    String[] s = valuesString.split(",");
-    List<Double> values = s.stream().map(element -> Double.valueOf(element));
-  }
-   private static String modifyJson(String json) {
-    try {
-      ObjectMapper objectMapper = new ObjectMapper();
-      JsonNode jsonNode = objectMapper.readTree(json);
-      ((ObjectNode) jsonNode).put("AEP_MW", "x");
 
-      return jsonNode.toString();
-    } catch (Exception e) {
-      // Handle any exception that occurred during JSON processing
-      e.printStackTrace();
-      return null; // or throw an exception if desired
-    }
+  private static List<Double> createValues(String valuesString) {
+    String[] s = valuesString.split(",");
+    List<String> strings = Arrays.asList(s);
+    return strings.stream().map(Double::valueOf).collect(Collectors.toList());
   }
 }
