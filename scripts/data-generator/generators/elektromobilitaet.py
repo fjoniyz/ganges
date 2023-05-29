cc645fdde5efacaa843fa4c21c3c158a1ebbf4e5
Maxim Popov <itsmail4max@gmail.com>
2023-05-29 20:42:42 +0200

    Decomposed generators into funcitons

--- scripts/data-generator/generators/elektromobilitaet.py
+++ scripts/data-generator/generators/elektromobilitaet.py
@@ -3,18 +3,31 @@ from datetime import datetime
 from random import uniform, randint
 from time import sleep
 import os
-# writes into a electromobilitydata.csv file with random secondly Endery Consumption
-# we added information about which apartment is affected
+from typing import Iterable
 
-with open(os.path.dirname(os.path.realpath(__file__)) + '/../data/emobilitydata.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    field = ["Timestamp","timeseries_id", "Seconds_EnergyConsumption"]
-    writer.writerow(field)
-    
+
+def generate_station_rows() -> Iterable:
+    """Returns value in format [Timestamp, timeseries_id, Seconds_EnergyConsumption]"""
     # timeseries = station
-    while True: 
-        for station in range(100):
-            date = datetime.now()
-            EV_usage = round(uniform(0, uniform(100, 1000)), 2)
-            writer.writerow([date, f"{station}" + "EMobility", EV_usage])
-        sleep(0.1)
+    for station in range(100):
+        date = datetime.now()
+        EV_usage = round(uniform(0, uniform(100, 1000)), 2)
+        yield [date, f"{station}" + "EMobility", EV_usage]
+        
+def get_fields_names():
+    return ["Timestamp","timeseries_id", "Seconds_EnergyConsumption"]
+    
+if __name__ == "__main__":
+    """ Writes into a electromobilitydata.csv file with random secondly 
+    Endery Consumption we added information about which apartment is affected"""
+    
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/../data/emobilitydata.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        field = get_fields_names()
+        writer.writerow(field)
+        
+        while True:
+            for row in generate_station_rows():
+                writer.writerow(row)
+            sleep(0.1)
+            
--- scripts/data-generator/generators/sanierungspotenziale.py
+++ scripts/data-generator/generators/sanierungspotenziale.py
@@ -12,23 +12,31 @@ from time import sleep
 import numpy as np
 import os
 
-# there are about 100 apartments
-with open(os.path.dirname(os.path.realpath(__file__)) + '/../data/sanierungsdata.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    field = ["Timestamp","timeseries_id", "Seconds_EnergyConsumption", "location", "inhabitants", "development_status"]
-    writer.writerow(field)
-
+def generate_electricity_rows():
+    """Returns value in format [Datetime, Seconds_EnergyConsumption, apartement_number, location]"""
+    # there are about 100 apartments
     # order of magnitude is completely wrong
-    # apartments is an array with the nuber of inhabitants
-    apartments = np.random.randint(1,6,100)
+    # apartments is an array with the number of inhabitants
+    apartments = np.random.randint(1,6,100) 
     locations = ["street a", "street b", "street c", "street c"]
     locations = int(len(apartments)/len(locations) + 1)*locations
     development_status = ["Old Construction", "New Construction"]
     development_status = int(len(apartments)/len(development_status) + 1)*development_status
+    for development, location, apartment, inhabitants in zip(development_status, locations, range(1, 100), apartments):
+        date = datetime.now()
+        # the number of inhabitants effects energy consumption
+        EV = round(uniform(1000*inhabitants, 10000*inhabitants), 2)
+        yield [date, f"{apartment}"+"sanierung-apartment", EV, location, inhabitants, development]
+        
+def get_fields_names():
+    return ["Datetime","Seconds_EnergyConsumption", "apartment_number", "location"]
+
+with open(os.path.dirname(os.path.realpath(__file__)) + '/../data/sanierungsdata.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = get_fields_names()
+    writer.writerow(field)
+
     while True:   
-        for development, location, apartment, inhabitants in zip(development_status, locations, range(1, 100), apartments):
-            date = datetime.now()
-            # the number of inhabitants effects energy consumption
-            EV = round(uniform(1000*inhabitants, 10000*inhabitants), 2)
-            writer.writerow([date, f"{apartment}"+"sanierung-apartment", EV, location, inhabitants, development])
-        sleep(0.1)
+        for row in generate_electricity_rows():
+            writer.writerow(row)
+            sleep(0.1)
