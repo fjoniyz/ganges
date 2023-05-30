a7111049618d3183d8f547cb4c67292dcf537a8d
Ralfons-06 <rafael.dobler@yahoo.de>
2023-05-30 13:15:41 +0200

    Added pertubation with laplace distribution

--- src/main/java/com/ganges/lib/castleguard/CastleGuard.java
+++ src/main/java/com/ganges/lib/castleguard/CastleGuard.java
@@ -2,16 +2,21 @@ package com.ganges.lib.castleguard;
 
 
 import java.util.*;
+import org.apache.commons.lang3.Range;
+import org.apache.commons.math3.distribution.LaplaceDistribution;
+import org.apache.commons.math3.random.JDKRandomGenerator;
 
 public class CastleGuard {
     private final CGConfig config;
-    private List<String> headers;
+    private Map<String, Range<Double>> headers;
     private String sensitiveAttr;
     private Deque<Item> items;
 
     public CastleGuard(CGConfig config, List<String> headers, String sensitiveAttr) {
         this.config = config;
-        this.headers = headers;
+        for (String header : headers) {
+            this.headers.put(header, Range.between(null, null));
+        }
         this.sensitiveAttr = sensitiveAttr;
     }
 
@@ -19,16 +24,16 @@ public class CastleGuard {
      * insert() in castle.py
      * @param data value tuple
      */
-    public void insertData(HashMap<String, Integer> data) {
+    public void insertData(HashMap<String, Double> data) {
         Random rand = new Random();
         if (config.isUseDiffPrivacy() && rand.nextDouble(1) > config.getBigBeta()) {
             return;
         }
-        Item item = new Item(data, this.headers, this.sensitiveAttr);
+        Item item = new Item(data, new ArrayList<>(this.headers.keySet()), this.sensitiveAttr);
         updateGlobalRanges(item);
 
         if (config.isUseDiffPrivacy()) {
-            item = perturb(item);
+            perturb(item);
         }
         Optional<Object> cluster = bestSelection(item);
         if (!cluster.isPresent()) {
@@ -67,11 +72,30 @@ public class CastleGuard {
 
     /**
      * fudge_tuple() in castle.py
-     * @return
      */
-    private Item perturb(Item item) {
-        // TODO: Implement
-        return null;
+    private void perturb(Item item) {
+        HashMap<String, Double> data = item.getData();
+
+        for (Map.Entry<String, Range<Double>> header : this.headers.entrySet()) {
+            // Check if header has a range
+            if (header.getValue().getMinimum() != null || header.getValue().getMaximum() != null) {
+                double max_value = header.getValue().getMaximum();
+                double min_value = header.getValue().getMinimum();
+
+                // Calaculate scale
+                double scale = Math.max((max_value - min_value), 1) / this.config.getPhi();
+                
+                // Draw random noise from Laplace distribution
+                JDKRandomGenerator rg = new JDKRandomGenerator();
+                LaplaceDistribution laplaceDistribution = new LaplaceDistribution(rg, 0, scale);
+                double noise = laplaceDistribution.sample();
+
+                // Add noise to original value
+                double original_value = data.get(header.getKey());
+                double perturbed_value = original_value + noise;
+                item.updateAttributes(header.getKey(), perturbed_value);
+            }
+        }
     }
 
     /**
--- src/main/java/com/ganges/lib/castleguard/Item.java
+++ src/main/java/com/ganges/lib/castleguard/Item.java
@@ -4,19 +4,23 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Item {
-    private HashMap<String, Integer> data;
+    private HashMap<String, Double> data;
     private List<String> headers;
     private String sensitiveAttr;
     private Object cluster;
 
-    public Item(HashMap<String, Integer> data, List<String> headers, String sensitiveAttr) {
+    public Item(HashMap<String, Double> data, List<String> headers, String sensitiveAttr) {
         this.data = data;
         this.headers = headers;
         this.sensitiveAttr = sensitiveAttr;
     }
 
-    public HashMap<String, Integer> getData() {
+    public HashMap<String, Double> getData() {
         return data;
+    }
+
+    public void updateAttributes(String header, Double value) {
+        this.data.put(header, value);
     }
 
     public List<String> getHeaders() {
