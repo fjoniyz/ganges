e8ea256854c90a4ab7f2e14a41cbf005e81659fd
sofiagreten123 <s.greten@campus.tu-berlin.de>
2023-06-08 18:12:14 +0200

    The tests for the informationLoss functions (one still fails)

--- src/test/java/com/ganges/lib/castleguard/ClusterTest.java
+++ src/test/java/com/ganges/lib/castleguard/ClusterTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.*;
+import org.apache.commons.lang3.Range;
 
 public class ClusterTest {
 
@@ -127,36 +128,150 @@ public class ClusterTest {
 
     @Test
     public void informationLossGivenTTest() {
+        ArrayList<String> headers = new ArrayList<>();
+        headers.add("timeseries_id");
+        headers.add("SecondsEnergyConsumption");
+        Cluster testCluster = new Cluster(headers);
+
+        HashMap<String, Range<Float>> globalRanges = new HashMap<>();
+        globalRanges.put(headers.get(0), Range.between(0.0F, 100.0F));
+        globalRanges.put(headers.get(1), Range.between(0.0F, 1000.0F));
+
+        HashMap<String, Float> dataOne = new HashMap<>();
+        dataOne.put(headers.get(0), 1.0F);
+        dataOne.put(headers.get(1), 200.0F);
+
+        Item one = new Item(dataOne,headers,null);
+
+        HashMap<String, Float> dataTwo = new HashMap<>();
+        dataTwo.put(headers.get(0), 5.0F);
+        dataTwo.put(headers.get(1), 300.0F);
+
+        Item two = new Item(dataTwo,headers,null);
+
+        HashMap<String, Float> dataThree = new HashMap<>();
+        dataThree.put(headers.get(0), 2.0F);
+        dataThree.put(headers.get(1), 250.0F);
+
+        Item three = new Item(dataThree,headers,null);
+
+        float loss1 = testCluster.informationLossGivenT(one, globalRanges);
+        Assert.assertEquals(0.0F, loss1, 0.0F);
+
+        testCluster.insert(one);
+
+        float loss2 = testCluster.informationLossGivenT(two, globalRanges);
+        Assert.assertEquals(0.14F, loss2, 0.0F);
+
+        testCluster.insert(two);
+
+        float loss3 = testCluster.informationLossGivenT(three, globalRanges);
+        Assert.assertEquals(0.14F, loss3, 0.0F);
     }
 
     @Test
     public void informationLossGivenCTest() {
+        ArrayList<String> headers = new ArrayList<>();
+        headers.add("timeseries_id");
+        headers.add("SecondsEnergyConsumption");
+        Cluster testClusterOne = new Cluster(headers);
+        Cluster testClusterTwo = new Cluster(headers);
+
+        HashMap<String, Range<Float>> globalRanges = new HashMap<>();
+        globalRanges.put(headers.get(0), Range.between(0.0F, 100.0F));
+        globalRanges.put(headers.get(1), Range.between(0.0F, 1000.0F));
+
+        HashMap<String, Float> dataOne = new HashMap<>();
+        dataOne.put(headers.get(0), 1.0F);
+        dataOne.put(headers.get(1), 200.0F);
+
+        Item one = new Item(dataOne,headers,null);
+
+        HashMap<String, Float> dataTwo = new HashMap<>();
+        dataTwo.put(headers.get(0), 5.0F);
+        dataTwo.put(headers.get(1), 300.0F);
+
+        Item two = new Item(dataTwo,headers,null);
+
+        HashMap<String, Float> dataThree = new HashMap<>();
+        dataThree.put(headers.get(0), 2.0F);
+        dataThree.put(headers.get(1), 250.0F);
+
+        Item three = new Item(dataThree,headers,null);
+
+        HashMap<String, Float> dataFour = new HashMap<>();
+        dataFour.put(headers.get(0), 2.0F);
+        dataFour.put(headers.get(1), 100.0F);
+
+        Item four = new Item(dataFour,headers,null);
+
+        float loss0 = testClusterOne.informationLossGivenC(testClusterTwo, globalRanges);
+        Assert.assertEquals(0.0F, loss0, 0.0F);
+
+        testClusterOne.insert(one);
+        float loss1 = testClusterOne.informationLossGivenC(testClusterTwo, globalRanges);
+        Assert.assertEquals(0.0F, loss1, 0.0F);
+
+        testClusterOne.insert(two);
+        float loss2 = testClusterOne.informationLossGivenC(testClusterTwo, globalRanges);
+        Assert.assertEquals(0.14F, loss2, 0.0F);
+
+        testClusterTwo.insert(three);
+        float loss3 = testClusterOne.informationLossGivenC(testClusterTwo, globalRanges);
+        Assert.assertEquals(0.14F, loss3, 0.0F);
+
+        testClusterTwo.insert(four);
+        float loss4 = testClusterOne.informationLossGivenC(testClusterTwo, globalRanges);
+        Assert.assertEquals(0.24F, loss4, 0.0F);
     }
 
     @Test
     public void informationLossTest() {
+        // EV_Usage from 100 to 1000 (according to the data generator)
+        // number of overall 100 stations
         //Create a cluster with 3 items inside
-        String[] array = {"timeseries_id","Seconds_EnergyConsumption"};
-        List<String> headers = Arrays.asList(array);
+
+        ArrayList<String> headers = new ArrayList<>();
+        headers.add("timeseries_id");
+        headers.add("SecondsEnergyConsumption");
         Cluster testCluster = new Cluster(headers);
-        HashMap<String, Float> data = new HashMap<>();
-        data.put("pid", 3.0F);
-        data.put("timeseries_id", 1.0F);
-        data.put("Seconds_EnergyConsumption", 2.0F);
 
-        Item testItem = new Item(data, headers,"Seconds_EnergyConsumption");
-        Item testItem2 = new Item(data, headers,"Seconds_EnergyConsumption");
-        Item testItem3 = new Item(data, headers,"Seconds_EnergyConsumption");
+        HashMap<String, Range<Float>> globalRanges = new HashMap<>();
+        globalRanges.put(headers.get(0), Range.between(0.0F, 100.0F));
+        globalRanges.put(headers.get(1), Range.between(0.0F, 1000.0F));
 
-        testCluster.insert(testItem);
-        testCluster.insert(testItem2);
-        testCluster.insert(testItem3);
+        HashMap<String, Float> dataOne = new HashMap<>();
+        dataOne.put(headers.get(0), 1.0F);
+        dataOne.put(headers.get(1), 200.0F);
 
-//        System.out.println(testCluster.getRanges());
-//
-//        Float loss = testCluster.information_loss(testCluster.getRanges());
-//
-//        System.out.println(loss);
+        Item one = new Item(dataOne,headers,null);
+
+        HashMap<String, Float> dataTwo = new HashMap<>();
+        dataTwo.put(headers.get(0), 5.0F);
+        dataTwo.put(headers.get(1), 300.0F);
+
+        Item two = new Item(dataTwo,headers,null);
+
+        HashMap<String, Float> dataThree = new HashMap<>();
+        dataThree.put(headers.get(0), 2.0F);
+        dataThree.put(headers.get(1), 250.0F);
+
+        Item three = new Item(dataThree,headers,null);
+
+        float loss0 = testCluster.informationLoss(globalRanges);
+        Assert.assertEquals(0.0F, loss0, 0.0F);
+
+        testCluster.insert(one);
+        float loss1 = testCluster.informationLoss(globalRanges);
+        Assert.assertEquals(0.0F, loss1, 0.0F);
+
+        testCluster.insert(two);
+        float loss2 = testCluster.informationLoss(globalRanges);
+        Assert.assertEquals(0.14F, loss2, 0.0F);
+
+        testCluster.insert(three);
+        float loss3 = testCluster.informationLoss(globalRanges);
+        Assert.assertEquals(0.14F, loss3, 0.0F);
     }
 
     @Test
