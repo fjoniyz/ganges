a1f568f996fa50423148945c815ef8d3e6ea9fcb
Yzeiri <fjoni.yzeiri@sap.com>
2023-07-18 21:15:52 +0200

    Revert pipe

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -1,20 +1,15 @@
 package myapps;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
-
-import java.util.stream.Collectors;
-import serdes.AnonymizedMessage;
-import serdes.Deserializer;
-import serdes.chargingstation.ChargingStationMessage;
 import serdes.Serializer;
+
+import serdes.chargingstation.ChargingStationMessage;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
@@ -23,53 +18,43 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.*;
-import serdes.emobility.EMobilityStationMessage;
-import serdes.emobility.EMobilityStationSerde;
 
 public class Pipe {
+    public static String processing(ChargingStationMessage value, String[] fields, DataRepository dataRepository) throws IOException {
+        double[] valuesList = createValuesList(fields, value);
+        StringBuilder valueToSaveInRedis = new StringBuilder();
+        for (double d: valuesList
+        ) {
+            valueToSaveInRedis.append(d).append(",");
+        }
+        // Retrieve non-anonymized data from cache
+        dataRepository.saveValue(valueToSaveInRedis.toString());
+        List<String> allSavedValues = dataRepository.getValues();
 
-    public static AnonymizedMessage createMessage(Class<?> messageClass, Object... values) {
-      AnonymizedMessage message = null;
-      if (messageClass.equals(EMobilityStationMessage.class)) {
-//        message = new EMobilityStationMessage(values[0], values[1], values[2], values[3]);
-        return message;
-      }
-      else {
-        return null; // TODO: add other message types
-      }
-    }
+        // Parse cached strings into double arrays
+        double[][] input = new double[allSavedValues.size()][];
 
+        for (int i = 0; i < allSavedValues.size(); i++) {
+            String[] values = allSavedValues.get(i).split(",");
+            double[] toDouble =
+                    Arrays.stream(values).mapToDouble(Double::parseDouble).toArray();
 
-    public static <T extends AnonymizedMessage> String processing(T message, DataRepository dataRepository, String[] fields) throws IOException {
-      EMobilityStationMessage eMobilityStationMessage = (EMobilityStationMessage) message;
-      String id = message.getId();
-      Double[] valuesList = message.getValuesListByKeys(fields);
-      dataRepository.open();
+            input[i] = toDouble;
+        }
 
-      // Retrieve non-anonymized data from cache
-      HashMap<String, Double> keyValueMap = new HashMap<>();
-      for (int i = 0; i < valuesList.length; i++) {
-        keyValueMap.put(fields[i], valuesList[i]);
-      }
-      dataRepository.saveValues(id, keyValueMap);
-
-      // Here we assume that for one message type the values are stored consistently
-      List<Map<String, Double>> allSavedValues = dataRepository.getValuesByKeys(fields);
-      dataRepository.close();
-
-      // Anonymization
-      Doca doca = new Doca();
-      List<Map<String, Double>> output = doca.anonymize(allSavedValues);
-      Double evUsage = output.get(output.size()-1).get("evUsage");
-      eMobilityStationMessage.setEvUsage(evUsage);
-
-      String outputString =
-          output.stream().map(dataRow -> dataRow.values().toString()).collect(Collectors.joining(
-              ","));
-      Serializer<T> serializer = new Serializer<T>();
-      byte[] json = serializer.serialize("output-test", (T) eMobilityStationMessage);
-      System.out.println("result: " + new String(json, StandardCharsets.UTF_8));
-      return new String(json, StandardCharsets.UTF_8);
+        // Anonymization
+        Doca docaInstance = new Doca();
+        double[][] output = docaInstance.anonymize(input);
+        double[] lastItem = output[output.length-1];
+        System.out.println("Last item: " + Arrays.deepToString(output));
+        float kwh = (float)lastItem[0];
+        int load_potential = (int) lastItem[1];
+        value.setKwh(kwh);
+        value.setLoadingPotential(load_potential);
+//        String result = Arrays.deepToString(output);
+        Serializer<ChargingStationMessage> serializer = new Serializer<>();
+        byte[] json = serializer.serialize("output-test", value);
+        return new String(json, StandardCharsets.UTF_8);
     }
 
     public static String[] getFieldsToAnonymize() throws IOException {
@@ -124,6 +109,7 @@ public class Pipe {
                     break;
                 default:
                     System.out.println("Invalid field in config file: " + field);
+                    break;
             }
         }
         return values.stream().mapToDouble(d -> d).toArray();
@@ -131,57 +117,48 @@ public class Pipe {
 
     public static void main(final String[] args) {
         String userDirectory = System.getProperty("user.dir");
-        Properties props = new Properties();
-
-        try (InputStream inputStream = Files.newInputStream(Paths.get(userDirectory + "/src/main/resources/pipe.properties"))) {
-          props.load(inputStream);
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-
-        String inputTopic = props.getProperty("input-topic");
-        String outputTopic = props.getProperty("output-topic");
-
-        Serializer<EMobilityStationMessage> serializer = new Serializer<>();
-        Deserializer<EMobilityStationMessage>
-            emobilityDeserializer = new Deserializer<>(EMobilityStationMessage.class);
-        Serde<EMobilityStationMessage> emobilitySerde = Serdes.serdeFrom(serializer, emobilityDeserializer);
-
         try (InputStream inputStream = Files.newInputStream(Paths.get(userDirectory + "/src/main/resources/kafka.properties"))) {
-          props.load(inputStream);
-          props.put(StreamsConfig.METADATA_MAX_AGE_CONFIG, "1000"); // Needed to prevent timeouts during broker startup.
-          props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-          props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, emobilitySerde.getClass());
+            Properties props = new Properties();
+            String inputTopic = "input-test7";
+            String outputTopic = "output";
 
-          // Create anonymization stream and use it with Kafka
-          StreamsBuilder streamsBuilder = new StreamsBuilder();
-          KStream<String, EMobilityStationMessage> src = streamsBuilder.stream(inputTopic, Consumed.with(Serdes.String(), emobilitySerde));
-          Produced<String, String> produced = Produced.with(Serdes.String(), Serdes.String());
+            Serializer<ChargingStationMessage> chargingStationSerializer = new Serializer<>();
+            Deserializer<ChargingStationMessage> chargingStationDeserializer = new serdes.Deserializer<>(ChargingStationMessage.class);
+            Serde<ChargingStationMessage> chargingStationMessageSerde = Serdes.serdeFrom(chargingStationSerializer, chargingStationDeserializer);
 
-          DataRepository dataRepository = new DataRepository();
-          String[] fields = getFieldsToAnonymize();
-          src.mapValues(value -> {
-              try {
-                  return processing(value, dataRepository, fields);
-              } catch (IOException e) {
-                  throw new RuntimeException(e);
-              }
-          }).to(outputTopic, produced);
-          Topology topology = streamsBuilder.build();
+            props.load(inputStream);
+            props.put(StreamsConfig.METADATA_MAX_AGE_CONFIG, "1000"); // Needed to prevent timeouts during broker startup.
+            props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+            props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, chargingStationMessageSerde.getClass());
 
-        try (KafkaStreams streams = new KafkaStreams(topology, props)) {
-          final CountDownLatch latch = new CountDownLatch(1);
-          try {
-            streams.start();
-            latch.await();
-          } catch (Throwable e) {
-            System.exit(1);
-          }
-        }
-        System.exit(0);
+            // Create anonymization stream and use it with Kafka
+            StreamsBuilder streamsBuilder = new StreamsBuilder();
+            KStream<String, ChargingStationMessage> src = streamsBuilder.stream(inputTopic, Consumed.with(Serdes.String(), chargingStationMessageSerde));
+            DataRepository dataRepository = new DataRepository();
+            Produced<String, String> produced = Produced.with(Serdes.String(), Serdes.String());
+            String[] fields = getFieldsToAnonymize();
+            src.mapValues(value -> {
+                try {
+                    return processing(value, fields, dataRepository);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).to(outputTopic, produced);
+            Topology topology = streamsBuilder.build();
 
+            try (KafkaStreams streams = new KafkaStreams(topology, props)) {
+                final CountDownLatch latch = new CountDownLatch(1);
+
+                try {
+                    streams.start();
+                    latch.await();
+                } catch (Throwable e) {
+                    System.exit(1);
+                }
+            }
+            System.exit(0);
         } catch (IOException e) {
-          throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
     }
 }
