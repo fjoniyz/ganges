851896d2b80ed3e30b1ab64f732a01bcb1888f40
Maxim Popov <itsmail4max@gmail.com>
2023-08-29 04:51:26 +0200

    added producer timestamp writing

--- src/main/java/benchmark/BenchmarkConsumer.java
+++ src/main/java/benchmark/BenchmarkConsumer.java
@@ -49,11 +49,13 @@ public class BenchmarkConsumer {
           if (record.value().isArray()) {
             for (JsonNode node : record.value()) {
               String id = node.get(idKey).textValue();
+              metricsCollector.setProducerTimestamps(node);
               metricsCollector.setConsumerTimestamps(id, consumerTimestamp);
             }
             metricsCollector.sendCurrentResultsToRemote();
           } else {
             String id = record.value().get(idKey).textValue();
+            metricsCollector.setProducerTimestamps(record.value());
             metricsCollector.setConsumerTimestamps(id, consumerTimestamp);
             metricsCollector.sendCurrentResultsToRemote();
           }
