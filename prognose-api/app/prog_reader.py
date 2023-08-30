6d2d56880745f9283e31b785d7a0b05c164dcb6d
flow <git@lowspace.de>
2023-08-30 19:54:51 +0200

    Update containerized prognosis

--- prognose-api/app/prog_reader.py
+++ prognose-api/app/prog_reader.py
@@ -1,7 +1,6 @@
 from confluent_kafka import Consumer
 import json
-import calendar
-import time
+import math
 
 import app.prognose as prognose
 
@@ -10,15 +9,42 @@ bootstrap_servers = 'broker:29092'
 group_id = 'consumer-group-1'
 
 
-def create_TaskSimEvCharging(x, power):
-    min_start = int(min(x["start_time_loading"]) / 16581777)
-    max_start = int(max(x["start_time_loading"]) / 16581777)
+def get_min_duration(messages):
+    min_duration = math.inf
+    for msg in messages:
+        if ((msg["end_time_loading"] - msg["start_time_loading"]) < min_duration):
+            min_duration = msg["end_time_loading"] - msg["start_time_loading"]
+    return min_duration
 
-    min_duration = int(min(x["duration"]))
-    max_duration = int(max(x["duration"]))
 
-    min_demand = int(min(x["kwh"]))
-    max_demand = int(max(x["kwh"]))
+def get_max_duration(messages):
+    max_duration = -math.inf
+    for msg in messages:
+        if ((msg["end_time_loading"] - msg["start_time_loading"]) > max_duration):
+            max_duration = msg["end_time_loading"] - msg["start_time_loading"]
+    return max_duration
+
+
+def create_TaskSimEvCharging(messages, power):
+    # each max is just min value plus one hour
+
+    min_start = math.inf
+    min_duration = math.inf
+    min_demand = math.inf
+    max_start = -math.inf
+    max_demand = -math.inf
+    max_duration = -math.inf
+
+    min_start = min([(msg["start_time_loading"]) for msg in messages])
+    max_start = max([(msg["start_time_loading"]) for msg in messages])
+
+    min_duration = get_min_duration(messages)
+    max_duration = get_max_duration(messages)
+    # print("Min duration: ", min_duration)
+    # print("Max duration: ", max_duration)
+
+    min_demand = min([(msg["kwh"]) for msg in messages])
+    max_demand = max([(msg["kwh"]) for msg in messages])
 
     return prognose.TaskSimEvCharging(min_duration, max_duration, min_demand, max_demand, min_start, max_start, power)
 
@@ -42,75 +68,46 @@ def generate_prognose(topic):
     consumer.subscribe([topic])
 
     try:
-        # We want 60 messages
+        # We want 10 messages
         messages = []
 
-        while len(messages) < 30:
+        while len(messages) < 10:
             # Poll for messages
             response = consumer.poll(1.0)
             if response is None:
                 continue
-            messages.append(response)
+            messages.append(json.loads(response.value().decode('utf-8')))
 
-        for i in range(len(messages)):
-            messages[i] = json.loads(messages[i].value().decode('utf-8'))
+        power = [11.0, 22.0]
+        task_instance = create_TaskSimEvCharging(messages, power)
 
-        power = [1, 2, 3, 4]
-        data_set = {}
-        data_set["start_time_loading"] = []
-        data_set["end_time_loading"] = []
-        data_set["kwh"] = []
-
-        for message in messages:
-            data_set["start_time_loading"] += [calendar.timegm(
-                time.strptime(message["start_time_loading"], '%Y-%m-%d %H:%M:%S'))]
-            data_set["end_time_loading"] += [calendar.timegm(
-                time.strptime(message["end_time_loading"], '%Y-%m-%d %H:%M:%S'))]
-            data_set["kwh"] += [int(message["kwh"])]
-
-        data_set["duration"] = [
-            x - y for x, y in zip(data_set["end_time_loading"], data_set["start_time_loading"])]
-
-        task_instance = create_TaskSimEvCharging(data_set, power)
-
-        """
-        print(f"Task instance: {task_instance}")
-        print(f"max_demand: {task_instance.max_demand}")
-        print(f"min_demand: {task_instance.min_demand}")
-        print(f"max_duration: {task_instance.max_duration}")
-        print(f"min_duration: {task_instance.min_duration}")
-        print(f"max_start: {task_instance.max_start}")
-        print(f"min_start: {task_instance.min_start}")
-        """
-
-        d = {
-            "col1": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col2": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col3": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col4": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col5": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col7": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col8": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col9": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
-                     task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col10": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+        messages = []
+        d = {"col1": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
                       task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-            "col11": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+             "col2": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
                       task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
-        }
+             "col3": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                      task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col4": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                      task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col5": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                      task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col7": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                      task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col8": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                      task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col9": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                      task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col10": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                       task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             "col11": [task_instance.max_start, task_instance.min_start, task_instance.min_demand,
+                       task_instance.max_demand, task_instance.min_duration, task_instance.max_duration],
+             }
 
         df = prognose.DataFrame(data=d)
 
         result = prognose.simulate_ev_forecast(
             df=df, cfg=task_instance)  # type: ignore
-        print(f"Result: {result}")
 
         return result.to_json()
 
